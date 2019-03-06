09a127ac8b84cf1689956d21d363a8d2ae73cdc6
Dmitry <gmyrak@gmail.com>
2019-03-06 17:49:13 +0200

    init

--- pages/views.py
+++ pages/views.py
@@ -2,9 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-
-
-
+# to github
 
 def homePageView(request):
     return HttpResponse("Hello World!")
